72f9371b07da7795ffd45a0dd816624f66b4e6ca
mohalw44 <mohalw04@gmail.com>
2025-05-05 20:02:55 +0300

    Resolve conflicts

--- builder/test_builder.go
+++ builder/test_builder.go
@@ -1,4 +1,4 @@
-package test_builder
+package builder
 
 import (
 	"bytes"
--- main.go
+++ main.go
@@ -7,45 +7,12 @@ import (
 
 func main() {
 
-	builder1 := test_builder.CreateNewBuilder()
-	builder1.
-		AddStaticNode(
-			"https://httpbin.org/json",
-			models.GET,
-			nil,
-		).
-		AddMatchStoreConstraint(
-			"slideshow.author",
-			"Yours Truly",
-			models.TypeString,
-			"authorName",
-		).
-		AddDynamicNode("https://openlibrary.org/search.json", models.GET,
-			func(m *map[string]models.TypedVariable) url.Values {
-				key := (*m)["authorName"]
-				params := url.Values{}
-				params.Set("q", key.Value.(string))
-				return params
-			}, nil).
-		AddExistConstraint("docs[2].author_key[0]", models.TypeString).
-		AddMatchStoreConstraint(
-			"docs[2].author_key[0]",
-			"OL3783157A",
-			models.TypeString,
-			"authorKey")
-
-	builder1.AddDynamicNodeBranch(
-		"https://httpbin.org/get",
-		models.GET,
-		func(m *map[string]models.TypedVariable) url.Values {
-			authorKey := (*m)["authorKey"]
-			params := url.Values{}
-			params.Set("author_key", authorKey.Value.(string))
-			return params
-		},
-		nil,
-	).AddExistConstraint("args.author_key", models.TypeString)
-
+	source :=
+		`
+StaticNode("https://github.com", GET, ExistConstraint(),
+	StaticNode(), 1000
+)
+		`
 
 	s := scanner.CreateScanner(source)
 	s.Scan()
