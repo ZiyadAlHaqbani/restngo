d3c29eda40a335e7b5e3e7a57441e4228917b00d
Rayan Ghabashi <108281084+Rayan-Ghabashi@users.noreply.github.com>
2025-05-12 20:53:41 +0300

    add match constraint and tests (#53)
    Co-authored-by: Rayan Ghabashi <rayan.ghabashi@trendyol.com>

---
+++ nodes/static_node_test.go
@@ -0,0 +1,44 @@
+package nodes_test
+
+import (
+	"htestp/nodes"
+	"net/http"
+	"testing"
+)
+
+type TestStaticNode struct {
+	*nodes.StaticNode
+	*http.Client
+}
+
+func TestExecuteStaticNode(t *testing.T) {
+
+}
+
+func TestCheckStaticNode(t *testing.T) {
+
+}
+
+func TestGetRespStaticNode(t *testing.T) {
+
+}
+
+func TestAddConstraintStaticNode(t *testing.T) {
+
+}
+
+func TestAddNodeStaticNode(t *testing.T) {
+
+}
+
+func TestGetNextNodesStaticNode(t *testing.T) {
+
+}
+
+func TestToStringStaticNode(t *testing.T) {
+
+}
+
+func TestSuccessfulStaticNode(t *testing.T) {
+
+}
