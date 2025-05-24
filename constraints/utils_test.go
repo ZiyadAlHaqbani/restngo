ada27d4c41ef312635583e17c8315b219168f4e3
ZiyadHQ <ziyadhaqbani@gmail.com>
2025-05-25 01:09:22 +0300

    added tests for partial traversal of complex objects and matching them

--- constraints/utils_test.go
+++ constraints/utils_test.go
@@ -31,15 +31,9 @@ func TestTraverse(t *testing.T) {
 
 func TestPartialTraverse(t *testing.T) {
 
-	test_json, err := os.ReadFile("../assets/test_load.json")
+	JSON, err := loadJSONFromFile("../assets/test_load.json")
 	if err != nil {
-		assert.Error(t, err, "error reading test_load.json")
-	}
-
-	var JSON interface{}
-	err = json.Unmarshal(test_json, &JSON)
-	if err != nil {
-		assert.Error(t, err, "error unmarhsalling test_load.json")
+		assert.Error(t, err, "expected to load json from file")
 	}
 
 	assert.Panics(t, func() {
@@ -58,20 +52,39 @@ func TestPartialTraverse(t *testing.T) {
 	})
 	assert.NotNil(t, res, "expected call to match with value 'Zamokuhle Zulu', %q", msg)
 
-	match_string := `
-	{
-    "name": "Bhupesh Menon","language": "Hindi","id": "0CEPNRDV98KT3ORP",
-    "bio": "Maecenas tempus neque ut porttitor malesuada. Phasellus massa ligula, hendrerit eget efficitur eget, tincidunt in ligula. Quisque mauris ligula, efficitur porttitor sodales ac, lacinia non ex. Maecenas quis nisi nunc.",
-    "version": 2.69}`
-	var match_json map[string]interface{}
-
-	err = json.Unmarshal([]byte(match_string), &match_json)
+	JSON, err = loadJSONFromFile("../assets/test_load_2.json")
 	if err != nil {
-		assert.Error(t, err, "error unmarhsalling test_load.json")
+		assert.Error(t, err)
 	}
+	JSON_part, err := loadJSONFromFile("../assets/test_load_2_users.json")
+	if err != nil {
+		assert.Error(t, err)
+	}
+
+	res, msg = partialTraverse_value("users", JSON, &models.TypedVariable{
+		Value: JSON_part,
+		Type:  models.TypeArray,
+	})
+	assert.NotNil(t, res, msg)
 
 }
 
 func TestParseJSONPath(t *testing.T) {
 
+}
+
+func loadJSONFromFile(filepath string) (interface{}, error) {
+
+	file, err := os.ReadFile(filepath)
+	if err != nil {
+		return nil, err
+	}
+
+	var JSON interface{}
+	err = json.Unmarshal(file, &JSON)
+	if err != nil {
+		return nil, err
+	}
+
+	return JSON, nil
 }
