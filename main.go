0b8de109add7111898c9f69ff0b1312603c82341
ZiyadAlHaqbani <ziyadhaqbani@gmail.com>
2025-05-26 14:51:55 +0300

    added a test to main

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"htestp/builder"
 	"htestp/models"
+	"net/url"
 )
 
 func main() {
@@ -11,7 +12,15 @@ func main() {
 	builder := builder.CreateNewBuilder()
 
 	builder.AddStaticNode("https://openlibrary.org/search.json?q=test", models.GET, nil).
-		AddFindStoreConstraint("title", models.TypeString, "test_var")
+		AddFindStoreConstraint("title", models.TypeString, "test_var").
+		AddDynamicNode("https://openlibrary.org/search.json", models.GET,
+			func(ctx *map[string]models.TypedVariable) url.Values {
+				params := url.Values{}
+				if variable := (*ctx)["test_var"]; variable.Type == models.TypeString {
+					params.Add("q", variable.Value.(string))
+				}
+				return params
+			}, nil)
 
 	println(builder.Run())
 
